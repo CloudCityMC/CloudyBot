8ba65e55aa3ae169c35707eaa43f5e003be47f6d
Chewsterchew <chew@chew.pw>
2017-12-28 13:05:36 -0600

    add rules link

--- plugins/links.rb
+++ plugins/links.rb
@@ -3,8 +3,13 @@ class Links
 
   match /forums/, method: :forums
   match /forum/, method: :forums
+  match /rules/, method: :rules
 
   def forums(m)
     m.reply 'You can chat on the forums here: http://forums.cloudcitymc.us'
+  end
+
+  def rules(m)
+    m.reply 'You can read the rules here: http://cloudcitymc.us/rules'
   end
 end
