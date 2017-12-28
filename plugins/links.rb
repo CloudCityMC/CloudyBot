f9e0eb053607b9d5c47f65824b9cd91ba8ed4811
Chewsterchew <chew@chew.pw>
2017-12-28 13:06:25 -0600

    add vote link

--- plugins/links.rb
+++ plugins/links.rb
@@ -4,6 +4,7 @@ class Links
   match /forums/, method: :forums
   match /forum/, method: :forums
   match /rules/, method: :rules
+  match /vote/, method: :vote
 
   def forums(m)
     m.reply 'You can chat on the forums here: http://forums.cloudcitymc.us'
@@ -11,5 +12,9 @@ class Links
 
   def rules(m)
     m.reply 'You can read the rules here: http://cloudcitymc.us/rules'
+  end
+
+  def vote(m)
+    m.reply 'You can find the vote links here: https://forums.cloudcitymc.us/threads/official-server-vote-links.5/'
   end
 end
