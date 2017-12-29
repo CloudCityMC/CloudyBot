44b1c84620d9bd44a50745dd29a6289beb2f5b83
Chew <chew@chew.pw>
2017-12-28 19:55:49 -0600

    Add irc link

--- plugins/links.rb
+++ plugins/links.rb
@@ -1,10 +1,10 @@
 class Links
   include Cinch::Plugin
 
-  match /forums/, method: :forums
   match /forum/, method: :forums
   match /rules/, method: :rules
   match /vote/, method: :vote
+  match /irc/, method: :irc
 
   def forums(m)
     m.reply 'You can chat on the forums here: http://forums.cloudcitymc.us'
@@ -16,5 +16,9 @@ class Links
 
   def vote(m)
     m.reply 'You can find the vote links here: https://forums.cloudcitymc.us/threads/official-server-vote-links.5/'
+  end
+
+  def irc(m)
+    m.reply 'You can chat with us on IRC here: http://cloudcitymc.us/irc'
   end
 end
