9414aa5453c04ffb978106e78106b3b77195d408
Chewsterchew <chew@chew.pw>
2018-01-14 09:01:06 -0600

    Use new "url shortener" for cloudcitymc and also "http" + "s"

--- plugins/links.rb
+++ plugins/links.rb
@@ -18,31 +18,31 @@ class Links
   match /links/, method: :links
 
   def forums(m)
-    m.reply 'You can chat on the forums here: http://forums.cloudcitymc.us'
+    m.reply 'You can chat on the forums here: https://forums.cloudcitymc.us'
   end
 
   def rules(m)
-    m.reply 'You can read the rules here: http://cloudcitymc.us/rules'
+    m.reply 'You can read the rules here: https://cloudcitymc.us/rules'
   end
 
   def vote(m)
-    m.reply 'You can find the vote links here: https://forums.cloudcitymc.us/threads/official-server-vote-links.5/'
+    m.reply 'You can find the vote links here: https://cloudcitymc.us/vote'
   end
 
   def irc(m)
-    m.reply 'You can chat with us on IRC here: http://cloudcitymc.us/irc'
+    m.reply 'You can chat with us on IRC here: https://cloudcitymc.us/irc'
   end
 
   def report(m)
-    m.reply 'You can report players on the forums here: https://forums.cloudcitymc.us/forums/report-players.10/'
+    m.reply 'You can report players on the forums here: https://cloudcitymc.us/report'
   end
 
   def sell(m)
-    m.reply 'Want to report a missing item from /sell? Do it on the forums here: https://forums.cloudcitymc.us/threads/items-that-cant-be-should-but-should.9'
+    m.reply 'Want to report a missing item from /sell? Do it on the forums here: https://cloudcitymc.us/sell'
   end
 
   def shop(m)
-    m.reply 'Want to suggest an item to be added to the shop? Do it on the forums here: https://forums.cloudcitymc.us/threads/items-that-should-be-sold-in-the-shop.10/'
+    m.reply 'Want to suggest an item to be added to the shop? Do it on the forums here: https://cloudcitymc.us/shop'
   end
 
   def store(m)
